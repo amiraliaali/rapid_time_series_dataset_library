7b6bf2d485a548e9346871be41ba2bb7065517aa
Marius Kaufmann <marius.kaufmann@rwth-aachen.de>
2025-08-17 16:10:43 +0200

    return tensors if tensors were passed

--- python/wrapper.py
+++ python/wrapper.py
@@ -12,10 +12,23 @@ class RustForecastingDataSet(ForecastingDataSet):
         val_prop: float,
         test_prop: float,
     ):
-        if isinstance(data, torch.Tensor):
+        was_tensor = isinstance(data, torch.Tensor)
+        if was_tensor:
             data = transform_to_numpy(data)
 
-        return super().__new__(cls, data, train_prop, val_prop, test_prop)
+        self = super().__new__(cls, data, train_prop, val_prop, test_prop)
+
+        self._was_tensor = was_tensor
+
+        return self
+
+    def collect(self, past_window: int, future_horizon: int, stride: int):
+        res = super().collect(past_window, future_horizon, stride)
+
+        if self._was_tensor:
+            res = _to_tensor(res)
+
+        return res
 
 
 class RustClassificationDataSet(ClassificationDataSet):
@@ -27,12 +40,32 @@ class RustClassificationDataSet(ClassificationDataSet):
         val_prop: float,
         test_prop: float,
     ):
-        if isinstance(data, torch.Tensor):
+        was_tensor = isinstance(data, torch.Tensor) and isinstance(labels, torch.Tensor)
+
+        if not was_tensor and (
+            isinstance(data, torch.Tensor) or isinstance(labels, torch.Tensor)
+        ):
+            raise TypeError(
+                "Inconsistent tensor types: both data and labels must be tensors or numpy arrays."
+            )
+
+        if was_tensor:
             data = transform_to_numpy(data)
-        if isinstance(labels, torch.Tensor):
             labels = transform_to_numpy(labels)
 
-        return super().__new__(cls, data, labels, train_prop, val_prop, test_prop)
+        self = super().__new__(cls, data, labels, train_prop, val_prop, test_prop)
+
+        self._was_tensor = was_tensor
+
+        return self
+
+    def collect(self):
+        res = super().collect()
+
+        if self._was_tensor:
+            res = _to_tensor(res)
+
+        return res
 
 
 def transform_to_numpy(
@@ -48,3 +81,19 @@ def check_tensor_is_double(data: torch.Tensor):
             f"Tensor is not of type double (float64). Actual dtype: {data.dtype}. "
             "You can convert it using `data = data.to(torch.float64)`."
         )
+
+
+def _to_tensor(obj):
+    """
+    Recursively convert numpy.ndarray to torch.Tensor.
+    Leave other objects untouched.
+    """
+    if isinstance(obj, np.ndarray):
+        tensor = torch.from_numpy(obj)
+        return tensor
+    elif isinstance(obj, tuple):
+        return tuple(_to_tensor(item) for item in obj)
+    elif isinstance(obj, list):
+        return [_to_tensor(item) for item in obj]
+    else:
+        return obj
